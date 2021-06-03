e9df64f35414efb3de23b5bcb9226e73c76bae1b
julietazon <juubozon@gmail.com>
2021-06-03 11:20:21 -0300

    improve theme switch + add bio

--- src/index.js
+++ src/index.js
@@ -164,40 +164,40 @@ checkbox.addEventListener("change", () => {
 */
 
 /* Theme change 2 */
-// Get the theme toggle input
+
 const themeToggle = document.querySelector("#flexSwitchCheckChecked");
-// Function that will switch the theme based on the if the theme toggle is checked or not
+// switch the theme based on the if the theme toggle is checked or not
 function switchTheme(theme) {
   if (theme.target.checked) {
-    document.documentElement.setAttribute("data-theme", "light");
-  } else {
     document.documentElement.setAttribute("data-theme", "dark");
+  } else {
+    document.documentElement.setAttribute("data-theme", "light");
   }
 }
-// Add an event listener to the theme toggle, which will switch the theme
+// event listener to the theme toggle - switch the theme
 themeToggle.addEventListener("change", switchTheme, false);
 
 function switchTheme(theme) {
   if (theme.target.checked) {
     document.documentElement.setAttribute("data-theme", "light");
 
-    // Set the user's theme preference to light
+    // user's theme preference to light
     localStorage.setItem("theme", "light");
   } else {
     document.documentElement.setAttribute("data-theme", "dark");
 
-    // Set the user's theme preference to dark
+    // user's theme preference to dark
     localStorage.setItem("theme", "dark");
   }
 }
 
-// Get the current theme from local storage
+// get current theme from local storage
 const currentTheme = localStorage.getItem("theme");
 // If the current local storage item can be found
 if (currentTheme) {
-  // Set the body data-theme attribute to match the local storage item
+  // body data-theme attribute match the local storage item
   document.documentElement.setAttribute("data-theme", currentTheme);
-  // If the current theme is dark, check the theme toggle
+
   if (currentTheme === "light") {
     themeToggle.checked = true;
   }
