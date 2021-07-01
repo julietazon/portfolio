12eb17f84108a3de26ba091303a1ef6e3d6e1a03
julietazon <juubozon@gmail.com>
2021-07-01 10:57:56 -0300

    updates

--- src/index.js
+++ src/index.js
@@ -203,4 +203,17 @@ if (currentTheme) {
   }
 }
 
+/* Type */
+var i = 0;
+var txt = "Lorem ipsum typing effect!"; /* The text */
+var speed = 50; /* The speed/duration of the effect in milliseconds */
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("demo").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
 /* Language */
